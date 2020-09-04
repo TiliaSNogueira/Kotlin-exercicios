e2170c2d92dca07d09aec89c2b0bd3d29677af6b
TiliaSNogueira <tiliasalomao@gmail.com>
2020-09-04 12:14:35 -0300

    incluindo exercicio 5

--- src/Exercicios/Exercicio5.kt
+++ src/Exercicios/Exercicio5.kt
@@ -1,2 +1,16 @@
 package Exercicios
 
+fun main() {
+    print(analisaNumeros(20, 18, 10, 15))
+}
+
+fun analisaNumeros( numeroA: Int, numeroB: Int, numeroC: Int, numeroD: Int) : Boolean{
+
+    if (numeroA > numeroC && numeroA > numeroD){
+        return true
+    } else if (numeroB > numeroC && numeroB > numeroD) {
+        return true
+    } else {
+        return false
+    }
+}
